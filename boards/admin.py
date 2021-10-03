6024ec2e23239a75a7bc1800f60515fd45d7a12d
Soumyajit <roy.valmont@gmail.com>
2021-10-03 19:47:49 +0530

    altered migrations

--- boards/admin.py
+++ boards/admin.py
@@ -5,3 +5,42 @@
 
 from django.contrib import admin
 
+from .models import Customer, Product, Cart, OrderPlaced
+
+# Now in the admin.py module, we register our Models to be managed in the Adminsitrative panel
+# By convention, Model name is joined with "Admin"
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'locality', 'city', 'zipcode', 'boards']
+    
+    # list_display is a pre-built attribute
+    # list_display tells Django, which model attributes to display in the admin panel
+    
+
+class ProductAdmin(admin.ModelAdmin):
+    list_displa = [
+        'title','selling_price', 'discount_price', 'descriptions',
+        'brand', 'category'
+    ]
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = [
+        'user', 'product', 'quantity'
+    ]
+
+
+class OrderPlacedAdmin(admin.ModelAdmin):
+    list_display=[
+        'user', 'product', 'quantity', 'ordered_date', 'status'
+    ]
+
+
+# NOW, we need to register the Model Admins created here with their corresponding Models
+# This tells Django, which admins to associate with which models
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(OrderPlaced, OrderPlacedAdmin)
--- plates/admin.py
+++ plates/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Plates
 
 # Register your models here.
+
+
+class PlateAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'description', 'price',
+        'plate_images', 'category'
+    ]
+
+admin.site.register(Plates, PlateAdmin)
