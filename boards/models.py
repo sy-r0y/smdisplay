85202af125658a625216db0fb6366db5ab943baf
Soumyajit <roy.valmont@gmail.com>
2021-10-03 18:59:53 +0530

    created migrations

--- boards/models.py
+++ boards/models.py
@@ -1,7 +1,70 @@
 # models.py module used to define classes or new types for modelling the concept of this app 'board'
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 # board size, board fabric, board type
+
+BOARD_CHOICES=(
+    ('magnetic','MagnetiC Board'), ('nonmagnetic','Non-Magnetic'),
+    ('marker','Marker Board'), ('pin','Soft Board'),
+    ('easel','Easel Board'), ('glass','Glass Board')
+) 
+CATEGORY_CHOICES=(
+    ('magnetic','Magnetic'), ('nonmangetic','Non-Magnetic'),
+    ('pin','Soft Board'), ('marker','Marker Board'),
+    ('mdf', 'MDF Board')
+)
+
+STATUS_CHOICES= (
+    ('a','accepted'), ('p','packed'), 
+    ('otw','on the way'), ('d','delivered'), 
+    ('c','cancel')
+)
+
+class Customer(models.Model):
+    # Customer model would have many-to-one relation with User
+
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    name= models.CharField(max_length=200)
+    locality= models.CharField(max_length=200)
+    city= models.CharField(max_length=50)
+    zipcode= models.IntegerField()
+    boards= models.CharField(choices=BOARD_CHOICES, max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Product(models.Model):
+    title= models.CharField(max_length=100)
+    selling_price= models.FloatField()
+    discount_price= models.FloatField()
+    description= models.TextField()
+    brand= models.CharField(max_length=100)
+    category= models.CharField(choices=CATEGORY_CHOICES, max_length=20)
+    product_image= models.ImageField(upload_to= 'productimg')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderPlaced(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    Customer= models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product= models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity= models.PositiveIntegerField(default=1)
+    ordered_date= models.DateTimeField(auto_now_add=True)
+    status= models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending')
+
--- plates/models.py
+++ plates/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+PLATE_TYPE=(
+    ('c', 'curved'),
+    ('f', 'flat'),
+    ('a', 'aluminum'),
+    ('w', 'wooden')
+)
+
+class Plates(models.Model):
+    name= models.CharField(max_length=50)
+    description= models.TextField()
+    price= models.FloatField()
+    plate_images= models.ImageField(upload_to='plateimg')
+    category= models.CharField(choices=PLATE_TYPE, max_length=20)
+
+    def __str__(self):
+        return str(self.id)
+
